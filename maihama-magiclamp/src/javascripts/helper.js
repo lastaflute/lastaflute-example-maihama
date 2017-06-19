fec9214dca99899dbfa9a30eb13388ca0723165d
black-trooper <hermanmiller.dkr@gmail.com>
2017-06-19 23:50:21 +0900

    change to es2015

--- maihama-magiclamp/src/javascripts/helper.js
+++ maihama-magiclamp/src/javascripts/helper.js
@@ -1,45 +1,46 @@
-window.helper = {};
+export default class Helper{
 
-window.helper.joinQueryParams = function(queryParams) {
-  var queries = [];
-  
-  for (var key in queryParams) {
-    if (queryParams[key] !== "") {
-      queries.push(key + "=" + queryParams[key]);
-    }
-  }      
-  if (queries.length > 0) {
-    return "?" + queries.join("&");
-  } else {
-    return "";
-  }
-}
-
-window.helper.mappingQueryParams = function() {
-  var raw = location.search;
-  if (raw === "") {
-    return [];
-  }
-  var splitParams = raw.replace("?", "").split("&")
-  var queryParams = {};
-  splitParams.forEach(function(p) {
-    var split = p.split("=");
-    queryParams[split[0]] = split[1]; // simple for example
-  });
-  return queryParams;
-}
-
-window.helper.formatMoneyComma = function(money) { // simple for example
-  var inner = function(input, output) {
-    if (input.length > 0) {
-      if (input.length % 3 == 0 && output.length > 0) {
-        return inner(input.substr(1), output + "," + input[0]);
-      } else {
-        return inner(input.substr(1), output + input[0]);
+  joinQueryParams(queryParams) {
+    const queries = [];
+    
+    for (const key in queryParams) {
+      if (queryParams[key] !== "") {
+        queries.push(key + "=" + queryParams[key]);
       }
+    }      
+    if (queries.length > 0) {
+      return "?" + queries.join("&");
     } else {
-      return output;
+      return "";
     }
-  };
-  return inner(money.toString(), "");
+  }
+
+  mappingQueryParams() {
+    const raw = location.search;
+    if (raw === "") {
+      return [];
+    }
+    const splitParams = raw.replace("?", "").split("&")
+    const queryParams = {};
+    splitParams.forEach(p => {
+      const split = p.split("=");
+      queryParams[split[0]] = split[1]; // simple for example
+    });
+    return queryParams;
+  }
+
+  formatMoneyComma(money) { // simple for example
+    const inner = (input, output) => {
+      if (input.length > 0) {
+        if (input.length % 3 == 0 && output.length > 0) {
+          return inner(input.substr(1), output + "," + input[0]);
+        } else {
+          return inner(input.substr(1), output + input[0]);
+        }
+      } else {
+        return output;
+      }
+    };
+    return inner(money.toString(), "");
+  }
 }
