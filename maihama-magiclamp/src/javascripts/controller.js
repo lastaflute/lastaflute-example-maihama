817b8e577ef2924a7585823e67454b0eba76e7ca
black-trooper <hermanmiller.dkr@gmail.com>
2017-06-27 09:41:36 +0900

    remove console.log()

--- maihama-magiclamp/src/javascripts/controller.js
+++ maihama-magiclamp/src/javascripts/controller.js
@@ -27,8 +27,6 @@ route('/member/add', () => {
   riot.mount('content', 'member-add');
 });
 route('/member/purchase/*..', (memberId) => {
-  console.log(route.query());
-  console.log(memberId);
   const params = route.query();
   params.memberId = memberId;
   riot.mount('content', 'member-purchase-list', params);
