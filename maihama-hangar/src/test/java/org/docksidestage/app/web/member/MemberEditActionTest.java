e84b7c65e443582bbe9d76fc0ef3548ca4ce3d39
black-trooper <hermanmiller.dkr@gmail.com>
2017-07-05 19:47:54 +0900

    add tests for member

---
+++ maihama-hangar/src/test/java/org/docksidestage/app/web/member/MemberEditActionTest.java
@@ -0,0 +1,62 @@
+package org.docksidestage.app.web.member;
+
+import javax.annotation.Resource;
+
+import org.dbflute.helper.HandyDate;
+import org.dbflute.optional.OptionalEntity;
+import org.dbflute.utflute.lastaflute.mock.TestingJsonData;
+import org.docksidestage.dbflute.allcommon.CDef;
+import org.docksidestage.dbflute.exbhv.MemberBhv;
+import org.docksidestage.dbflute.exentity.Member;
+import org.docksidestage.unit.UnitHangarTestCase;
+import org.lastaflute.web.response.JsonResponse;
+
+public class MemberEditActionTest extends UnitHangarTestCase {
+
+    @Resource
+    MemberBhv memberBhv;
+
+    public void test_index() {
+        // ## Arrange ##
+        MemberEditAction action = new MemberEditAction();
+        inject(action);
+        Integer memberId = 1;
+
+        // ## Act ##
+        JsonResponse<MemberEditBody> response = action.index(memberId);
+
+        // ## Assert ##
+        showJson(response);
+        TestingJsonData<MemberEditBody> data = validateJsonData(response);
+        assertEquals(data.getJsonResult().memberId, memberId);
+    }
+
+    public void test_update() {
+        // ## Arrange ##
+        MemberEditAction action = new MemberEditAction();
+        inject(action);
+
+        MemberEditBody editBody = memberBhv.selectByPK(1).map(entity -> {
+            MemberEditBody body = new MemberEditBody();
+            body.memberId = entity.getMemberId();
+            body.versionNo = entity.getVersionNo();
+            return body;
+        }).get();
+        editBody.memberName = "test user";
+        editBody.memberAccount = "test_user0001";
+        editBody.birthdate = new HandyDate("2017-07-05").getLocalDate();
+        editBody.memberStatus = CDef.MemberStatus.Provisional;
+
+        // ## Act ##
+        action.update(editBody);
+
+        // ## Assert ##
+        OptionalEntity<Member> optionalEntity = memberBhv.selectByPK(1);
+        assertTrue(optionalEntity.isPresent());
+        Member member = optionalEntity.get();
+        assertEquals(member.getMemberName(), editBody.memberName);
+        assertEquals(member.getMemberAccount(), editBody.memberAccount);
+        assertEquals(member.getBirthdate(), editBody.birthdate);
+        assertEquals(member.getMemberStatusCodeAsMemberStatus(), editBody.memberStatus);
+    }
+}
---
+++ maihama-hangar/src/test/java/org/docksidestage/app/web/member/MemberListActionTest.java
@@ -0,0 +1,187 @@
+package org.docksidestage.app.web.member;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.annotation.Resource;
+
+import org.dbflute.helper.HandyDate;
+import org.dbflute.optional.OptionalThing;
+import org.dbflute.utflute.lastaflute.mock.TestingJsonData;
+import org.docksidestage.app.web.base.paging.SearchPagingResult;
+import org.docksidestage.dbflute.allcommon.CDef;
+import org.docksidestage.dbflute.exbhv.PurchaseBhv;
+import org.docksidestage.unit.UnitHangarTestCase;
+import org.lastaflute.web.exception.RequestIllegalTransitionException;
+import org.lastaflute.web.response.JsonResponse;
+import org.lastaflute.web.validation.exception.ValidationErrorException;
+
+/**
+ * @author black-trooper
+ */
+public class MemberListActionTest extends UnitHangarTestCase {
+
+    @Resource
+    private PurchaseBhv purchaseBhv;
+
+    public void test_index_all_condition() {
+        // ## Arrange ##
+        MemberListAction action = new MemberListAction();
+        inject(action);
+        MemberSearchBody body = new MemberSearchBody();
+        body.memberName = "s";
+        body.purchaseProductName = "a";
+        body.memberStatus = CDef.MemberStatus.Formalized;
+        body.unpaid = true;
+        body.formalizedFrom = new HandyDate("2006-09-04").getLocalDate();
+        body.formalizedTo = new HandyDate("2006-09-04").getLocalDate();
+
+        // ## Act ##
+        JsonResponse<SearchPagingResult<MemberSearchRowResult>> response = action.index(OptionalThing.of(1), body);
+
+        // ## Assert ##
+        showJson(response);
+        TestingJsonData<SearchPagingResult<MemberSearchRowResult>> data = validateJsonData(response);
+        List<MemberSearchRowResult> rows = data.getJsonResult().rows;
+        assertHasAnyElement(rows);
+        rows.forEach(bean -> {
+            assertContainsIgnoreCase(bean.memberName, body.memberName);
+            assertEquals(bean.memberStatusName, body.memberStatus.alias());
+            assertTrue(bean.formalizedDate.isAfter(body.formalizedFrom) || bean.formalizedDate.isEqual(body.formalizedFrom));
+            assertTrue(bean.formalizedDate.isBefore(body.formalizedTo.plusDays(1)));
+        });
+
+        List<Integer> memberIdList = rows.stream().map(bean -> bean.memberId).collect(Collectors.toList());
+        Set<Integer> unpaidMemberIds = purchaseBhv.selectList(cb -> {
+            cb.specify().columnMemberId();
+            cb.query().setMemberId_InScope(memberIdList);
+            cb.query().setPaymentCompleteFlg_Equal_False();
+        }).stream().map(bean -> bean.getMemberId()).collect(Collectors.toSet());
+        memberIdList.forEach(memberId -> {
+            assertTrue(unpaidMemberIds.contains(memberId));
+        });
+
+        Set<Integer> purchaseMemberIds = purchaseBhv.selectList(cb -> {
+            cb.specify().columnMemberId();
+            cb.query().setMemberId_InScope(memberIdList);
+            cb.query().queryProduct().setProductName_LikeSearch(body.purchaseProductName, op -> op.likeContain());
+        }).stream().map(bean -> bean.getMemberId()).collect(Collectors.toSet());
+        memberIdList.forEach(memberId -> {
+            assertTrue(purchaseMemberIds.contains(memberId));
+        });
+    }
+
+    public void test_index_formalizedFrom() {
+        // ## Arrange ##
+        MemberListAction action = new MemberListAction();
+        inject(action);
+        MemberSearchBody body = new MemberSearchBody();
+        body.formalizedFrom = new HandyDate("2006-09-04").getLocalDate();
+
+        // ## Act ##
+        JsonResponse<SearchPagingResult<MemberSearchRowResult>> response = action.index(OptionalThing.of(1), body);
+
+        // ## Assert ##
+        showJson(response);
+        TestingJsonData<SearchPagingResult<MemberSearchRowResult>> data = validateJsonData(response);
+        List<MemberSearchRowResult> rows = data.getJsonResult().rows;
+        assertHasAnyElement(rows);
+        rows.forEach(bean -> {
+            assertTrue(bean.formalizedDate.isAfter(body.formalizedFrom) || bean.formalizedDate.isEqual(body.formalizedFrom));
+        });
+    }
+
+    public void test_index_formalizedTo() {
+        // ## Arrange ##
+        MemberListAction action = new MemberListAction();
+        inject(action);
+        MemberSearchBody body = new MemberSearchBody();
+        body.formalizedTo = new HandyDate("2006-09-04").getLocalDate();
+
+        // ## Act ##
+        JsonResponse<SearchPagingResult<MemberSearchRowResult>> response = action.index(OptionalThing.of(1), body);
+
+        // ## Assert ##
+        showJson(response);
+        TestingJsonData<SearchPagingResult<MemberSearchRowResult>> data = validateJsonData(response);
+        List<MemberSearchRowResult> rows = data.getJsonResult().rows;
+        assertHasAnyElement(rows);
+        rows.forEach(bean -> {
+            assertTrue(bean.formalizedDate.isBefore(body.formalizedTo.plusDays(1)));
+        });
+    }
+
+    public void test_index_all_data() {
+        // ## Arrange ##
+        MemberListAction action = new MemberListAction();
+        inject(action);
+        MemberSearchBody body = new MemberSearchBody();
+
+        // ## Act ##
+        JsonResponse<SearchPagingResult<MemberSearchRowResult>> response = action.index(OptionalThing.of(1), body);
+
+        // ## Assert ##
+        showJson(response);
+        TestingJsonData<SearchPagingResult<MemberSearchRowResult>> data = validateJsonData(response);
+        List<MemberSearchRowResult> rows = data.getJsonResult().rows;
+        assertHasAnyElement(rows);
+        rows.forEach(bean -> {});
+    }
+
+    public void test_index_notFound() {
+        // ## Arrange ##
+        MemberListAction action = new MemberListAction();
+        inject(action);
+        MemberSearchBody body = new MemberSearchBody();
+        body.purchaseProductName = "not found";
+
+        // ## Act ##
+        JsonResponse<SearchPagingResult<MemberSearchRowResult>> response = action.index(OptionalThing.of(1), body);
+
+        // ## Assert ##
+        showJson(response);
+        TestingJsonData<SearchPagingResult<MemberSearchRowResult>> data = validateJsonData(response);
+        assertHasZeroElement(data.getJsonResult().rows);
+    }
+
+    public void test_index_validate_request() {
+        // ## Arrange ##
+        MemberListAction action = new MemberListAction();
+        inject(action);
+        MemberSearchBody body = new MemberSearchBody();
+        body.memberName = "12345"; // max = 5
+        body.purchaseProductName = "1234567890"; // max = 10
+
+        // ## Act ##
+        action.index(OptionalThing.of(1), body);
+    }
+
+    public void test_index_validate_request_error() {
+        // ## Arrange ##
+        MemberListAction action = new MemberListAction();
+        inject(action);
+        MemberSearchBody body = new MemberSearchBody();
+        body.memberName = "123456"; // max = 5
+        body.purchaseProductName = "12345678901"; // max = 10
+
+        // ## Act ##
+        // ## Assert ##
+        assertException(ValidationErrorException.class, () -> {
+            action.index(OptionalThing.of(1), body);
+        });
+    }
+
+    public void test_search_bad_request() {
+        // ## Arrange ##
+        MemberListAction action = new MemberListAction();
+        inject(action);
+        MemberSearchBody body = new MemberSearchBody();
+
+        // ## Act ##
+        // ## Assert ##
+        assertException(RequestIllegalTransitionException.class, () -> {
+            action.index(OptionalThing.of(0), body);
+        });
+    }
+}
