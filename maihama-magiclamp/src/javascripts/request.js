b5bfbcd0caf7653baa9eb85b10b35c910f61a054
black-trooper <hermanmiller.dkr@gmail.com>
2017-08-08 21:18:06 +0900

    improve response error handling

--- maihama-magiclamp/src/javascripts/request.js
+++ maihama-magiclamp/src/javascripts/request.js
@@ -6,26 +6,15 @@ export default class Request {
 
   post(url, queryParams, onSuccess, onError, withoutAuthorization) {
     let _sa = sa.post(this.apiPrefix + url).send(queryParams);
-    if (!withoutAuthorization) {
-      const authkey = sessionStorage.getItem(RC.SESSION.auth.key);
-      _sa = _sa.set('x-authorization', authkey);
-    }
-    _sa.end((error, response) => {
-      if (response.ok) {
-        onSuccess(response);
-      }
-      else if (response.clientError && (response.body.cause === 'VALIDATION_ERROR' || response.body.cause === 'BUSINESS_ERROR')) {
-        onError(this.toValidationErros(response.body.errors));
-      }
-      else if (response.clientError && response.body.cause === 'LOGIN_REQUIRED') {
-        observable.trigger(RC.EVENT.route.change, '/');
-        observable.trigger(RC.EVENT.auth.sign, false);
-      }
-    });
+    this.request(_sa, onSuccess, onError, withoutAuthorization);
   }
 
   get(url, onSuccess, onError, withoutAuthorization) {
     let _sa = sa.get(this.apiPrefix + url);
+    this.request(_sa, onSuccess, onError, withoutAuthorization);
+  }
+
+  request(_sa, onSuccess, onError, withoutAuthorization) {
     if (!withoutAuthorization) {
       const authkey = sessionStorage.getItem(RC.SESSION.auth.key);
       _sa = _sa.set('x-authorization', authkey);
@@ -33,13 +22,20 @@ export default class Request {
     _sa.end((error, response) => {
       if (response.ok) {
         onSuccess(response);
-      }
-      else if (response.clientError && (response.body.cause === 'VALIDATION_ERROR' || response.body.cause === 'BUSINESS_ERROR')) {
+      } else if (response.clientError && response.body.cause === 'BUSINESS_ERROR') {
+        const hasLoginRequired = response.body.errors.some(error => {
+          return error.code === 'LOGIN_REQUIRED';
+        });
+        if (hasLoginRequired) {
+          observable.trigger(RC.EVENT.route.change, '/');
+          observable.trigger(RC.EVENT.auth.sign, false);
+        } else {
+          onError(this.toValidationErros(response.body.errors));
+        }
+      } else if (response.clientError && response.body.cause === 'VALIDATION_ERROR') {
         onError(this.toValidationErros(response.body.errors));
-      }
-      else if (response.clientError && response.body.cause === 'LOGIN_REQUIRED') {
-        observable.trigger(RC.EVENT.route.change, '/');
-        observable.trigger(RC.EVENT.auth.sign, false);
+      } else {
+        onError({ _global: ['Please retry!'] });
       }
     });
   }
