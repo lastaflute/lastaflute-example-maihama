6df9f9bb3a97230c2e1fed409e6a134bc60702df
jflute <dbflute@gmail.com>
2017-10-11 05:39:39 +0900

    test remogen

--- maihama-showbase/src/main/java/org/docksidestage/app/web/wx/remogen/WxRemogenListAction.java
+++ maihama-showbase/src/main/java/org/docksidestage/app/web/wx/remogen/WxRemogenListAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.docksidestage.app.web.base.ShowbaseBaseAction;
+import org.docksidestage.app.web.base.general.KeyValueResult;
 import org.docksidestage.dbflute.allcommon.CDef;
 import org.lastaflute.web.Execute;
 import org.lastaflute.web.response.JsonResponse;
@@ -17,6 +18,23 @@ public class WxRemogenListAction extends ShowbaseBaseAction {
     // ===================================================================================
     //                                                                             Execute
     //                                                                             =======
+    @Execute
+    public JsonResponse<List<KeyValueResult>> basic() {
+        List<KeyValueResult> reasonList = CDef.WithdrawalReason.listAll().stream().map(reason -> {
+            return new KeyValueResult(reason.code(), reason.alias());
+        }).collect(Collectors.toList());
+        return asJson(reasonList);
+    }
+
+    // #for_now forName() error in saving swagger
+    //@Execute
+    //public JsonResponse<List<ValueGenericsResult<String>>> generics() {
+    //    List<ValueGenericsResult<String>> reasonList = CDef.WithdrawalReason.listAll().stream().map(reason -> {
+    //        return new ValueGenericsResult<String>(reason.code(), reason.alias());
+    //    }).collect(Collectors.toList());
+    //    return asJson(reasonList);
+    //}
+
     @Execute
     public JsonResponse<List<SimpleEntry<String, String>>> getter() {
         List<SimpleEntry<String, String>> reasonList = CDef.WithdrawalReason.listAll().stream().map(reason -> {
