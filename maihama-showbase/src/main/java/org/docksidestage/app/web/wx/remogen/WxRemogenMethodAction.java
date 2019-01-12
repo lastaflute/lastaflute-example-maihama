6b1caaa3bae46d4f9aeb901a101f13504e67f34d
jflute <dbflute@gmail.com>
2019-01-12 19:26:42 +0900

    just fix: remove comment-out for RemoteApiGen target

--- maihama-showbase/src/main/java/org/docksidestage/app/web/wx/remogen/WxRemogenMethodAction.java
+++ maihama-showbase/src/main/java/org/docksidestage/app/web/wx/remogen/WxRemogenMethodAction.java
@@ -50,12 +50,11 @@ public class WxRemogenMethodAction extends ShowbaseBaseAction {
     }
 
     // DELETE http://localhost:8098/showbase/wx/remogen/method
-    // *now compiler error of RemoteApiGen, cannot determine query or on-body form by this definition
-    //@Execute
-    //public JsonResponse<SuperSimpleResult> delete$index(SuperSimpleForm form) {
-    //    logger.debug("form: {}", form);
-    //    return asJson(new SuperSimpleResult(form.sea, form.land));
-    //}
+    @Execute
+    public JsonResponse<SuperSimpleResult> delete$index(SuperSimpleForm form) {
+        logger.debug("form: {}", form);
+        return asJson(new SuperSimpleResult(form.sea, form.land));
+    }
 
     // DELETE http://localhost:8098/showbase/wx/remogen/method/enclosing
     @Execute
